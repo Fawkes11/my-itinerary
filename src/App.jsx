2e8a7364652a798e1c7d0da3d5bb6ebdd66a859a
Fawkes11 <hesslerinampuez@gmail.com>
2023-08-01 20:23:07 -0500

    create intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Header from "./components/Header";
+import Intro from "./components/Intro";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Intro />
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        prima: {
+          500: '#4F46E5',
+          700: '#3f3797'
+        }
+      }
+    },
+    fontFamily: {
+      'sans': ['Segoe UI', 'Arial', 'sans-serif'],
+    },
   },
   plugins: [],
 }
+
